72adda8fc7c5f06ade39dbd61f68f8e5e869d95c
Lindell Carter <lindellcarternyc@gmail.com>
2021-04-26 12:45:13 -0400

    rename files

---
+++ src/main/java/com/ttweb55/recipeapp/repository/UseremailRepository.java
@@ -0,0 +1,12 @@
+package com.ttweb55.recipeapp.repository;
+
+import com.ttweb55.recipeapp.models.Useremail;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * The CRUD Repository connecting Useremail to the rest of the application
+ */
+public interface UseremailRepository
+    extends CrudRepository<Useremail, Long>
+{
+}
